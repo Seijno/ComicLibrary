51c89e4158235013aa1fd719b18894a801f93613
SvenPennings <sven.pennings@gmail.com>
2022-12-20 14:00:54 +0100

    library + pdf

--- library.php
+++ library.php
@@ -7,26 +7,32 @@ if (empty($_SESSION)) {
     header("Location: login.php");
 }
 
-include "connect.php";
+function getBooks() {
 
-$query = $conn->prepare("SELECT * FROM library WHERE user_id = :id");
-$query->bindParam(":id", $_SESSION["id"]);
-$query->execute();
+    include "connect.php";
 
-$result = $query->fetch(PDO::FETCH_ASSOC);
+    $query = $conn->prepare("SELECT * FROM library WHERE user_id = :id");
+    $query->bindParam(":id", $_SESSION["id"]);
+    $query->execute();
 
-if ($result) {
-    $array = str_replace(", ", " ", $result["book_id"]);
-    $array = explode(" ", $array);
-    
-    foreach ($array as $book) {
-        $query = $conn->prepare("SELECT * FROM book WHERE id = :id");
-        $query->bindParam(":id", $book);
-        $query->execute();
+    $result = $query->fetch(PDO::FETCH_ASSOC);
 
-        $result = $query->fetch(PDO::FETCH_ASSOC);
+    if ($result) {
+        $array = str_replace(",", " ", $result["book_id"]);
+        $array = explode(" ", $array);
+        
+        foreach ($array as $book) {
+            $query = $conn->prepare("SELECT * FROM book WHERE id = :id");
+            $query->bindParam(":id", $book);
+            $query->execute();
 
-        echo $result["title"];
+            $result = $query->fetch(PDO::FETCH_ASSOC);
+
+            if ($result) {
+                $id = $result["id"];
+                echo "<a href='read_pdf.php?id=$id' target='_blank'>" . $result["title"] . "</a><br>";
+            }
+        }
     }
 }
 
@@ -41,6 +47,48 @@ if ($result) {
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="" method="post">
+        <input type="search" name="search" id="search">
+        <input type="submit" name="submit" value="Search">
+    </form>
+    <hr>
+    <?php 
+        if (!isset($_POST["submit"]) || $_POST["search"] == "") {
+            getBooks();
+        } 
+    ?>
 </body>
 </html>
+
+<?php
+
+include "connect.php";
+
+if (isset($_POST["submit"])) {
+    $query = $conn->prepare("SELECT * FROM library WHERE user_id = :id");
+    $query->bindParam(":id", $_SESSION["id"]);
+    $query->execute();
+
+    $result = $query->fetch(PDO::FETCH_ASSOC);
+
+    if ($result) {
+        $array = str_replace(",", " ", $result["book_id"]);
+        $array = explode(" ", $array);
+        
+        foreach ($array as $book) {
+            $query = $conn->prepare("SELECT * FROM book WHERE id = :id AND title = :title");
+            $query->bindParam(":id", $book);
+            $query->bindParam(":title", $_POST["search"]);
+            $query->execute();
+
+            $result = $query->fetch(PDO::FETCH_ASSOC);
+
+            if ($result) {
+                $id = $result["id"];
+                echo "<a href='read_pdf.php?id=$id' target='_blank'>" . $result["title"] . "</a><br>";
+            }
+        }
+    }
+}
+
+?>
