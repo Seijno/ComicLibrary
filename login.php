5e396efb7e10057b597d4c9caa07f71cc2b1992e
Seijno <seijnovannieuwkoop@gmail.com>
2023-01-18 11:52:39 +0100

    .

--- login.php
+++ login.php
@@ -1,32 +1,63 @@
-<?php
-
+<?php include "connect.php";
 // check if useris already logged in if so redirect to store page
 if (isset($_SESSION['username'])) {
     header('Location: store.php');
 }
-
-include_once("connect.php");
-?>
-
-<!DOCTYPE html>
+ ?>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <!-- Login form -->
-    <form action="login.php" method="post">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" required>
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" required>
-        <input type="submit" value="Login">
-    </form>
+<?php include "header.php"; ?>
+    <section id="loginform">
+        <div class="container">
+            <div class="justify-content-center row">
+                <div class="col-5 text-center" id="form">
+                    <p class="py-3">Welkom terug!</p>
+                    <form method="post" action="store.php">
+                        <label class="py-3" for="email">Email</label>
+                        <input type="email" id="email" name="email" placeholder="pieter@gmail.com" required>
+                        <br>
+                        <label class="py-3" for="password">Password</label>
+                        <input type="password" name="password" id="password" placeholder="password" required>
+                        <br>
+                        <button class="w-100 btn btn-lg btn-danger" type="submit">Sign in</button>
+                        <div class="text-center py-2">
+                            <p>New here? Create a account <a href="register.php">here</a>!</p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="py-5"></div>
+    <section id="footer">
+      <div class="container">
+        <div class="row justify-content-evenly">
+          <div class="col-4">
+            <div class="text-center">
+              <h6>Nieuwsbrief</h6>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="text-center">
+              <h6>Contact</h6>
+              <p>email: comiclibrary@support.com<br>
+                 telephone number: +31 6 12345678<br>
+                 adres: 2311WV Amsterdam<br>
+                 oosterhoekstraat 37</p>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="text-center">
+              <h6>Navigatie</h6>
+              <a href="#">upcoming</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
-    <?php
+<?php
+ include "footer.php";
     // Check if the username and password fields are set
     if (isset($_POST['username']) && isset($_POST['password'])) {
         // Check if the user is in the database
@@ -54,9 +85,5 @@ include_once("connect.php");
             echo "User does not exist";
         }
     }
-    ?>
-
-
-    
-</body>
-</html>
+?>
+<!-- commit -->
