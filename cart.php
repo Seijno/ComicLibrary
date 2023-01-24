70522ddb6361ede6b512808283f2671c7a60cfe2
BigT <sic7@tutanota.de>
2023-01-24 12:39:04 +0100

    Tabel & cover preview

--- cart.php
+++ cart.php
@@ -1,99 +1,154 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
-    <title>Cart</title>
-</head>
-<body>
-<h2>Cart</h2>
+<?php include "header.php"; ?>
 
-<form action='buy.php' method='post'>
-    <div id="cart"></div>
-    <div id="total"></div>
-</form>
+<div class="container">
+    <h2>Cart</h2>
+
+    <form action='checkout.php' method='post'>
+        <div id='cart' class="cart container py-4">
+            <div class="row justify-content-center">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th class="h5">Book</th>
+                                <th>Author</th>
+                                <th>Genre</th>
+                                <th>Price</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody id="cart-list">
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col mb-3">
+                <button type="submit" class="btn btn-primary my-3" name="buy">Checkout</button>
+            </div>
+            <div class="col text-end">
+                <p id="total" class=""></p>
+            </div>
+        </div>
+    </form>
+</div>
 
 <script>
-    // get books from localstorage
     var books = removeDuplicates(JSON.parse(localStorage.getItem('cart')));
-    var total = 0;
+    // create an array to store prices
+    var prices = [];
 
-    // check if cart is empty
-    if (books.length > 0) {
+    // Check if cart is empty
+    if (books != null && books.length > 0) {
+
         // loop through books and append them to the cart
         for (var i = 0; i < books.length; i++) {
-            var book = books[i];
-            var id = book.id;
-            var title = book.title;
-            var price = book.price;
+            var id = books[i].id;
 
-            total += price;
+            // request book data from database
+            var request = new XMLHttpRequest();
+            request.open("GET", "get_book.php?id=" + id, true);
+            request.send();
+            request.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    // parse data from json
+                    var book = JSON.parse(this.responseText)[0];
 
-            $('#cart').append(`
-                <div class="card">
-                    <h3>${title}</h3>
-                    <p>${price}</p>
-                    <input type="hidden" name="id" value="${id}">
-                    <button type="button" onclick="removeFromCart(${id})">Remove</button><br><br><br>
-                </div>
+                    // get price and add it to the prices array
+                    var price = parseFloat(book.price);
+                    prices.push(price);
+
+                    // display total price
+                    $('#total').html(`Total: €${prices.reduce((a, b) => a + b, 0)}`);
+
+                    // display book to on cart list
+                    $('#cart-list').append(`
+                        <tr>
+                            <th>
+                                <div class="d-flex align-items-center">
+                                    <img src="data:image/jpeg;base64, ${book.image}" class="img-fluid rounded-3" style="width: 120px;" alt="Book cover">
+                                    <div class="flex-column ms-4">
+                                        <p class="mb-2">${book.title}</p>
+                                        <p class="mb-0 text-secondary">${book.description}</p>
+                                    </div>
+                                </div>
+                            </th>
+                            <td class="align-middle">
+                                <p class="mb-0" style="font-weight: 500;">${book.author}</p>
+                            </td>
+                            <td class="align-middle">
+                                <p class="mb-0" style="font-weight: 500;">${book.genre}</p>
+                            </td>
+                            <td class="align-middle">
+                                <p class="mb-0" style="font-weight: 500;">€${book.price}</p>
+                            </td>
+
+                            <td class="align-middle">
+                                <input type="hidden" name="id" value="${id}">
+                                <a class='removeButton link-danger' onclick='removeFromCart(${book.id})'><i class='delete-btn bi bi-x fs-4'></i></a>
+                            </td>
+
+                        </tr>
+                        `);
+                    }
+                }
+            }
+        } else {
+            // replace cart with message and remove checkout button
+            $('#cart').html(`
+            <div class="alert alert-secondary" role="alert">
+                Your cart is empty!
+            </div>
             `);
+            $('button[name="buy"]').remove();
         }
 
-        // add buy button
-        $('#cart').append(`
-            <button type="submit" name="buy">Buy</button>
-        `);
+        // remove duplicate books from cart
+        function removeDuplicates(books) {
+            var newBooks = [];
+            
+            // if books is null return null
+            if (books == null) {
+                return null;
+            }
+            
+            for (var i = 0; i < books.length; i++) {
+                var book = books[i];
+                var duplicate = false;
 
-    } else {
-        // if cart is empty display message
-        $('#cart').append(`
-            <div class="card">
-                <h3>Cart is empty</h3>
-            </div>
-        `);
-    }
+                for (var j = 0; j < newBooks.length; j++) {
+                    var newBook = newBooks[j];
+                    if (book.id == newBook.id) {
+                        duplicate = true;
+                    }
+                }
 
-    // remove duplicate books from cart
-    function removeDuplicates(books) {
-        var newBooks = [];
-
-        for (var i = 0; i < books.length; i++) {
-            var book = books[i];
-            var duplicate = false;
-
-            for (var j = 0; j < newBooks.length; j++) {
-                var newBook = newBooks[j];
-                if (book.id == newBook.id) {
-                    duplicate = true;
+                if (!duplicate) {
+                    newBooks.push(book);
                 }
             }
 
-            if (!duplicate) {
-                newBooks.push(book);
-            }
+            return newBooks;
         }
 
-        return newBooks;
-    }
+        // remove book from cart
+        function removeFromCart(id) {
+            var books = JSON.parse(localStorage.getItem('cart'));
+            var newBooks = [];
 
-    // remove book from cart
-    function removeFromCart(id) {
-        var books = JSON.parse(localStorage.getItem('cart'));
-        var newBooks = [];
-
-        for (var i = 0; i < books.length; i++) {
-            var book = books[i];
-            if (book.id != id) {
-                newBooks.push(book);
+            for (var i = 0; i < books.length; i++) {
+                var book = books[i];
+                if (book.id != id) {
+                    newBooks.push(book);
+                }
             }
-        }
 
-        localStorage.setItem('cart', JSON.stringify(newBooks));
-        location.reload();
-    }
+            localStorage.setItem('cart', JSON.stringify(newBooks));
+            location.reload();
+        }
 </script>
 </body>
+
 </html>
