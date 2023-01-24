62f9a0e1f551c6940045a93bdc1e2f996b320768
BigT <sic7@tutanota.de>
2023-01-24 12:43:25 +0100

    Header include

--- overview.php
+++ overview.php
@@ -2,15 +2,7 @@
 include_once("connect.php");
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Overview</title>
-</head>
-<body>
+<?php include "header.php"; ?>
 
     <h2>Overview</h2>
     
